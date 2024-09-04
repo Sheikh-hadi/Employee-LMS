0c432577f73388de5e5a60464dc46b1fb136713d
Sadia7725 <sadiasafdar7725@gmail.com>
2024-09-04 12:37:43 +0500

    ++

--- src/components/AttendenceDashboard.js
+++ src/components/AttendenceDashboard.js
@@ -1,46 +1,45 @@
 import React from 'react';
 import { Row, Col, Card, Progress } from 'antd';
-
 import BirthdayCard from './BirthdayCard';
 
 const AttendenceDashboard = () => {
-
-
   return (
-    <Row gutter={16} style={{ marginTop: 20, }}>
-      <Col span={12} >
-        <Card style={{ height: "100%" }}>
+    <Row gutter={[16, 16]} style={{ marginTop: 20,}}>
+      {/* Today's Attendance Card */}
+      <Col xs={24} md={12}>
+        <Card style={{ height: '100%' }}>
           <h3>Today Attendance</h3>
-          <Row>
-            <Col span={5} style={{ textAlign: 'center', padding: '20px', borderRadius: '20px', backgroundColor: 'lightblue' }}>
-              <div style={{ fontSize: 24, color: 'green' }}>0</div>
-              <div><b>Present </b></div>
+          <Row gutter={[16, 10]}style={{ display: 'flex', justifyContent: 'space-between' }}>
+            <Col xs={20} sm={6}  style={{ textAlign: 'center', padding: '20px', borderRadius: '10px', backgroundColor: 'lightblue' , }}>
+              <div style={{ fontSize: 24, color: 'green',}}>0</div>
+              <div><b>Present</b></div>
             </Col>
-            <Col span={5} offset={1} style={{ textAlign: 'center', padding: '20px', borderRadius: '20px', backgroundColor: 'lightblue' }}>
+            <Col xs={20} sm={5} style={{ textAlign: 'center', padding: '20px', borderRadius: '10px', backgroundColor: 'lightblue' , }}>
               <div style={{ fontSize: 24, color: 'red' }}>0</div>
-              <div><b>Absent </b></div>
+              <div><b>Absent</b></div>
             </Col>
-            <Col span={5} offset={1} style={{ textAlign: 'center', padding: '20px', borderRadius: '20px', backgroundColor: 'lightblue' }}>
+            <Col xs={20} sm={4} style={{ textAlign: 'center', padding: '20px', borderRadius: '10px', backgroundColor: 'lightblue', }}>
               <div style={{ fontSize: 24, color: 'orange' }}>0</div>
-              <div><b> Half Day</b></div>
+              <div><b>Half Day</b></div>
             </Col>
-            <Col span={5} offset={1} style={{ textAlign: 'center', padding: '20px', borderRadius: '20px', backgroundColor: 'lightblue' }}>
+            <Col xs={20} sm={7} style={{ textAlign: 'center', padding: '20px', borderRadius: '10px', backgroundColor: 'lightblue' ,}}>
               <div style={{ fontSize: 24, color: 'gray' }}>0</div>
-              <div><b>Unmarked</b></div>
+              <div style={{ textAlign: 'left' }}><b>Unmarked</b></div>
             </Col>
           </Row>
         </Card>
       </Col>
 
-      <Col span={4}>
+      {/* March's Attendance Progress Card */}
+      <Col xs={24} md={6}>
         <Card>
-          <h3>March's Attendance</h3>
+          <h3 style={{ textAlign: 'left' }}>March's Attendance</h3>
           <Progress type="dashboard" percent={78} />
         </Card>
       </Col>
 
-      {/* Update this Card to show Birthdays */}
-      <Col span={8}>
+      {/* Birthday Card */}
+      <Col xs={24} md={6}>
         <BirthdayCard />
       </Col>
     </Row>
