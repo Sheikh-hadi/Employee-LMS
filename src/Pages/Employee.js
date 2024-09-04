36e6bb43b91153b7474403a14bdbac8772418c5a
Sadia7725 <sadiasafdar7725@gmail.com>
2024-09-04 15:24:18 +0500

    ++

--- src/Pages/Employee.js
+++ src/Pages/Employee.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import EmployeeTable from '../components/EmployeeTable'
 import AddNewEmployee from '../components/AddNewEmployee'
+import EmployeeList from '../components/EmployeeList'
 
 const Employee = () => {
   return (
     <div>
+      <EmployeeList/>
       <AddNewEmployee/>
       <EmployeeTable/>
 
