993cb3434f83648e3c97a004cba7ec8dc41cfb3f
Sadia7725 <sadiasafdar7725@gmail.com>
2024-09-06 16:53:52 +0500

    ++

--- src/Pages/Employee.js
+++ src/Pages/Employee.js
@@ -1,21 +1,28 @@
 import React from "react";
 import EmployeeTable from "../components/EmployeeTable";
 import AddNewEmployee from "../components/AddNewEmployee";
-import {Skeleton } from "antd";
+import { Skeleton } from "antd";
 import { useQuery } from "@tanstack/react-query";
 import axios from "axios";
+import { useEmployeeContext } from "../context/EmployeeContext";
 
 const Employee = () => {
+  const { employees, setEmployees } = useEmployeeContext(); // Use the hook here
   const { data, isLoading, isError, error } = useQuery({
     queryKey: ["employees"],
     queryFn: async () => {
       const response = await axios.get("http://localhost:4000/api/v1/employee");
-
-      return response.data; 
+      return response.data;
     },
   });
-  console.log("data: ", data?.data);
 
+  console.log("data: ", data);
+  console.log("employees: ", employees);
+  React.useEffect(() => {
+    if (data) {
+      setEmployees(data.data); // Ensure you access the correct data structure
+    }
+  }, [data, setEmployees]); // Added `data` to the dependency array
 
   if (isLoading) {
     return <Skeleton active />;
--- src/index.js
+++ src/index.js
@@ -4,13 +4,14 @@ import './index.css';
 import { QueryClientProvider, QueryClient } from '@tanstack/react-query';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { EmployeeProvider } from './context/EmployeeContext';
 
 const queryClient = new QueryClient();
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
-      <App />
+   <EmployeeProvider>  <App /></EmployeeProvider> 
     </QueryClientProvider>
   </React.StrictMode>
 );
