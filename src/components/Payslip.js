8ee6cf1c310904ac1cb5f298c1150dd85223a28e
Sadia7725 <sadiasafdar7725@gmail.com>
2024-09-13 17:59:25 +0500

    ++

--- src/components/Payslip.js
+++ src/components/Payslip.js
@@ -2,10 +2,18 @@ import React from "react";
 import { Row, Col } from "antd";
 
 const PaySlip = () => {
- 
   const tdStyle = {
     padding: "10px",
     textAlign: "left",
+    borderBottom: "1px solid #ccc", // Only bottom line for row separation
+  };
+
+  const tdCenterStyle = {
+    // padding: "10px",
+    // textAlign: "left",
+ textAlign: "center", 
+    
+  
     borderBottom: "1px solid #ccc", // Only bottom line for row separation
   };
 
@@ -21,22 +29,27 @@ const PaySlip = () => {
     fontWeight: "bold",
   };
 
-  
- 
   return (
-    <div style={{  padding: "20px",
-      maxWidth: "800px",
-      margin: "0 auto",
-      backgroundColor: "#f8f8f8",
-      borderRadius: "10px",
-      border: "1px solid #e5e5e5",}}>
-
+    <div
+      style={{
+        padding: "20px",
+        maxWidth: "800px",
+        margin: "0 auto",
+        backgroundColor: "#f8f8f8",
+        borderRadius: "10px",
+        border: "1px solid #e5e5e5",
+      }}
+    >
       {/* Existing Content */}
       {/* Header Row */}
-      <Row style={{ backgroundColor: "#4a4a4a",
-    padding: "20px",
-    color: "#fff",
-    borderRadius: "10px 10px 0 0",}}>
+      <Row
+        style={{
+          backgroundColor: "#4a4a4a",
+          padding: "20px",
+          color: "#fff",
+          borderRadius: "10px 10px 0 0",
+        }}
+      >
         <Col span={12}>
           <p>
             Name: <strong>Name here</strong>
@@ -54,63 +67,82 @@ const PaySlip = () => {
 
       {/* Salary Distribution Header */}
       <Row>
-        <Col span={24} style={{
-    padding: "10px 0",
-    textAlign: "center",
-    fontSize: "25px",
-    fontWeight: "bold",}}>
+        <Col
+          span={24}
+          style={{
+            padding: "10px 0",
+            textAlign: "center",
+            fontSize: "25px",
+            fontWeight: "bold",
+          }}
+        >
           Salary Distribution
         </Col>
       </Row>
 
       <Row>
         <Col span={24}>
-          <table style={{ width: "100%",
-    borderCollapse: "collapse",
-    marginTop: "10px",}}>
+          <table
+            style={{
+              width: "100%",
+              borderCollapse: "collapse",
+              marginTop: "10px",
+            }}
+          >
             <tbody>
               <tr>
                 <td style={tdStyle}>BASIC SALARY</td>
+                <td style={tdCenterStyle}>N</td>
                 <td style={tdRightStyle}>0.00</td>
               </tr>
               <tr>
                 <td style={tdStyle}>TRANSPORT</td>
+                <td style={tdCenterStyle}>N</td>
                 <td style={tdRightStyle}>0.00</td>
               </tr>
               <tr>
                 <td style={tdStyle}>MEDICAL ALLOWANCE</td>
+                <td style={tdCenterStyle}>N</td>
                 <td style={tdRightStyle}>0.00</td>
               </tr>
               <tr>
                 <td style={tdStyle}>HOUSING</td>
+                <td style={tdCenterStyle}>N</td>
                 <td style={tdRightStyle}>0.00</td>
               </tr>
               <tr>
                 <td style={tdStyle}>MEAL</td>
+                <td style={tdCenterStyle}>N</td>
                 <td style={tdRightStyle}>0.00</td>
               </tr>
               <tr>
                 <td style={tdStyle}>MOBILE ALLOWANCE</td>
+                <td style={tdCenterStyle}>N</td>
                 <td style={tdRightStyle}>0.00</td>
               </tr>
               <tr>
                 <td style={tdStyle}>ARREARS</td>
+                <td style={tdCenterStyle}>N</td>
                 <td style={tdRightStyle}>0.00</td>
               </tr>
               <tr>
                 <td style={tdStyle}>REWARD</td>
+                <td style={tdCenterStyle}>N</td>
                 <td style={tdRightStyle}>0.00</td>
               </tr>
               <tr>
                 <td style={tdStyle}>E. O. T. M</td>
+                <td style={tdCenterStyle}>N</td>
                 <td style={tdRightStyle}>0.00</td>
               </tr>
               <tr>
                 <td style={tdStyle}>BONUS</td>
+                <td style={tdCenterStyle}>N</td>
                 <td style={tdRightStyle}>0.00</td>
               </tr>
               <tr>
                 <td style={tdStyle}>INCENTIVE</td>
+                <td style={tdCenterStyle}>N</td>
                 <td style={tdRightStyle}>0.00</td>
               </tr>
             </tbody>
@@ -130,54 +162,69 @@ const PaySlip = () => {
 
       {/* Deductions Section */}
       <Row>
-        <Col span={24} style={{
-    padding: "10px 0",
-    textAlign: "center",
-    fontSize: "25px",
-    fontWeight: "bold",}}>
-         Deductions
+        <Col
+          span={24}
+          style={{
+            padding: "10px 0",
+            textAlign: "center",
+            fontSize: "25px",
+            fontWeight: "bold",
+          }}
+        >
+          Deductions
         </Col>
       </Row>
 
       <Row>
         <Col span={24}>
-          <table style={{ width: "100%",
-    borderCollapse: "collapse",
-    marginTop: "10px",}}>
+          <table
+            style={{
+              width: "100%",
+              borderCollapse: "collapse",
+              marginTop: "10px",
+            }}
+          >
             <tbody>
               <tr>
                 <td style={tdStyle}>Tax</td>
+                <td style={tdCenterStyle}>N</td>
                 <td style={tdRightStyle}>0.00</td>
               </tr>
               <tr>
                 <td style={tdStyle}>Provident Fund</td>
+                <td style={tdCenterStyle}>N</td>
                 <td style={tdRightStyle}>0.00</td>
               </tr>
               <tr>
                 <td style={tdStyle}>Advance/Loan</td>
+                <td style={tdCenterStyle}>N</td>
                 <td style={tdRightStyle}>0.00</td>
               </tr>
               <tr>
                 <td style={tdStyle}>Advance Salaries</td>
+                <td style={tdCenterStyle}>N</td>
                 <td style={tdRightStyle}>0.00</td>
               </tr>
               <tr>
                 <td style={tdStyle}>Absent</td>
+                <td style={tdCenterStyle}>N</td>
                 <td style={tdRightStyle}>0.00</td>
               </tr>
               <tr>
                 <td style={tdStyle}>Late Deductions</td>
+                <td style={tdCenterStyle}>N</td>
                 <td style={tdRightStyle}>0.00</td>
               </tr>
               <tr>
                 <td style={tdStyle}>Other Deductions</td>
+                <td style={tdCenterStyle}>N</td>
                 <td style={tdRightStyle}>0.00</td>
               </tr>
               <tr>
                 <td style={tdStyle}>Total Deductions</td>
+                <td style={tdCenterStyle}>N</td>
                 <td style={tdRightStyle}>0.00</td>
               </tr>
-        
             </tbody>
           </table>
         </Col>
@@ -194,11 +241,15 @@ const PaySlip = () => {
       </Row>
 
       <Row>
-        <Col span={24} style={{ padding: "20px 0",
-    textAlign: "center",
-    fontSize: "21px",
-    fontWeight:"900",
- }}>
+        <Col
+          span={24}
+          style={{
+            padding: "20px 0",
+            textAlign: "center",
+            fontSize: "21px",
+            fontWeight: "900",
+          }}
+        >
           Amount in Words
           <div style={{ fontWeight: "normal", marginTop: "10px" }}>
             Thirty two thousand five forty eighty Rupees Only
@@ -207,10 +258,15 @@ const PaySlip = () => {
       </Row>
 
       <Row>
-        <Col span={24} style={{  padding: "10px 0",
-    textAlign: "center",
-    fontSize: "16px",
-    color: "#666",}}>
+        <Col
+          span={24}
+          style={{
+            padding: "10px 0",
+            textAlign: "center",
+            fontSize: "16px",
+            color: "#666",
+          }}
+        >
           The figures herein are generated electronically. Hence, does not require signature except for alteration.
         </Col>
       </Row>
